88e57287732bf2a76efa0bf2f252f40d35c67349
Milishia <32485592+Milishia@users.noreply.github.com>
2019-01-14 12:06:29 +0100

    - Lille ændring

--- app/src/main/java/com/johansen/dk/madimage/activities/optionsActivity.java
+++ app/src/main/java/com/johansen/dk/madimage/activities/optionsActivity.java
@@ -95,8 +95,8 @@ public class optionsActivity extends AppCompatActivity implements View.OnClickLi
 
 
                 // delete stack
-                finish();
-                //supportFinishAfterTransition();
+                //finish();
+                supportFinishAfterTransition();
         }
     }
 }
